bee0ca956ca30c2861ebc187ae7028ef155d45e5
yashmvora <yashmvora18999@gmail.com>
2025-11-19 16:38:35 +0530

    assign the mock data to feed the websocket

--- StockPriceTracker/ContentView.swift
+++ StockPriceTracker/ContentView.swift
@@ -8,14 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var store: StockPricesStore
+
+    init() {
+        let url = URL(string: "wss://ws.postman-echo.com/raw")!
+        let service = WebSocketService(url: url)
+        let symbols = [
+            "AAPL","GOOG","TSLA","NVDA","MSFT","AMZN","META","NFLX","ADBE","AVGO",
+            "ORCL","INTC","AMD","SHOP","UBER","ABNB","SNOW","CRM","SQ","PLTR",
+            "SPOT","PYPL","COIN","BABA","NIO"
+        ]
+        _store = StateObject(wrappedValue: StockPricesStore(service: service, initialSymbols: symbols))
+    }
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        FeedView()
+            .environmentObject(store)
     }
 }
 
